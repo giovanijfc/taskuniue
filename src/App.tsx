3cd830448a643c35f777c49284e21ca46191308b
Alex Sandro <relex303@gmail.com>
2020-08-22 13:49:06 -0300

    rebase da develope

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import firebase from 'firebase/app';
 
 import firebaseConfig from './constants/firebaseConfig';
 
-function App() {
-  const [value, setValue] = useState('estado inicial value ');
+import LoginScreen from './screens/LoginScreen/LoginScreen';
 
+function App() {
   useLayoutEffect(() => {
     firebase.initializeApp(firebaseConfig);
   }, []);
-
-  return (
-    <div>
-      {value}
-      <button onClick={() => setValue('estado mudado')}>mudar valor</button>
-    </div>
-  );
+  return <LoginScreen />;
 }
 
 export default App;
