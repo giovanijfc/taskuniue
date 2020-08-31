a3bd1434125e8bebc5e21100e8c90d3783b474cd
unknown <giovanijfc@gmail.com>
2020-08-31 19:36:47 -0300

    adds tasks screen

---
+++ src/screens/TasksScreen/TasksScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+const TasksScreen = () => {
+  return <Styled.Container>home</Styled.Container>;
+};
+
+export default TasksScreen;
